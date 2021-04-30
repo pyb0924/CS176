e2070edfe921b1e5a01c9c5fe9414e4a3f3fdcad
pyb0924 <pyb0924@sjtu.edu.cn>
2021-04-30 09:44:44 +0800

    HW7 finished

--- lesson_7_homework/task2-server-cache/server.js
+++ lesson_7_homework/task2-server-cache/server.js
@@ -1,13 +1,40 @@
 const http = require('http')
+const fs = require("fs");
+const nanoid = require('nanoid')
+const port = 3000;
 
-const port = 3000
 
+const id = nanoid.nanoid()
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('你好世界\n')
+    let url = req.url.replace(/^\//, '');
+    if (!url) {
+        url = "index.html";
+    }
+    let param = url.split('?');
+    if (!fs.existsSync(param[0])) {
+        res.statusCode = 404;
+        res.end();
+    } else {
+        switch (param[1]) {
+            case "max-age":
+                res.writeHead(200, "OK", {"Cache-Control": "max-age=999"});
+                break;
+            case "no-cache":
+                if (req.headers['if-none-match'] === id) {
+                    res.statusCode = 304;
+                    res.end();
+                } else {
+                    res.writeHead(200, "OK", {"Cache-Control": "no-cache", "ETag": id});
+                }
+                break;
+            case "no-store":
+                res.writeHead(200, "OK", {"Cache-Control": "no-store"});
+                break;
+        }
+        fs.createReadStream(param[0]).pipe(res);
+    }
 })
 
 server.listen(port, () => {
-    console.log(`服务器运行在 http://localhost:${port}/`)
+    console.log(`正在运行 http://localhost:${port}/`)
 })
